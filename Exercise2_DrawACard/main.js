fd492c4e741056bc5ad45b2f008c02cad2049777
PatrikHogblom <patrik.hogblom@live.se>
2024-04-08 10:38:15 +0200

    created second prat of the exercise to draw a card using api from deck of cards api, main functionality is finished and styling is only thing left

---
+++ Exercise2_DrawACard/main.js
@@ -0,0 +1,24 @@
+document.getElementById("drawCard-btn").addEventListener('click', getApi);
+
+function getApi()
+{
+
+    let newShuffledDeck = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
+    //get a deck to use
+    fetch(newShuffledDeck)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        let cardImgSrc = data.cards[0].image;
+        showCardImgDiv(cardImgSrc);
+    })
+    .catch(err => console.error(err))
+
+}
+
+function showCardImgDiv(imageURL){
+    let newCardImageDiv = document.createElement("img");
+    newCardImageDiv.setAttribute("src",imageURL);
+    document.getElementById("cardImage").innerHTML = "";//nollställer diven
+    document.getElementById("cardImage").appendChild(newCardImageDiv);
+}
