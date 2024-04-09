e6061d69d4e4ed60b16e1a3fb92c2eae21f7602d
PatrikHogblom <patrik.hogblom@live.se>
2024-04-09 10:23:22 +0200

    small changes, added and changed text in starwars biometic exercise

--- Exercise1_StarWarsBiometrics/starWars.js
+++ Exercise1_StarWarsBiometrics/starWars.js
@@ -11,22 +11,20 @@ function getApi()
         return;
     }
 
-    //check if we get result Array[1] or Array[0], 1 is exists, 0 is not found 
-
 
     let urlToApi = "https://www.swapi.tech/api/people/?name=" + serachInput; // Using wildcard to match any sequence of characters
 
     fetch(urlToApi)
     .then(res => res.json())
     .then(data => {
-        console.log(data.result); 
-        if(data && data.result && data.result.length > 0)
+        console.log(data.result.length); 
+        if(data.result.length > 0)
         {
             let informationString = "";
             document.getElementById("outputInfo").style.color = "black";
             data.result.forEach(element => {
                 let character = element.properties;
-                informationString += `Name: ${character.name}, Height: ${character.height}, Mass: ${character.mass} Gender: ${character.gender}, Hair-color: ${character.hair_color}\n\n`;
+                informationString += `Name: ${character.name} , Height: ${character.height}, Mass: ${character.mass} Gender: ${character.gender}, Hair-color: ${character.hair_color}\n\n`;
             });
             document.getElementById("outputInfo").value = informationString;
         }
